823b25c0a783877cfcc480278cd99cc7183ba499
Varun Adhikari <adhikari.varun92@gmail.com>
2018-12-07 22:56:45 +0530

    adding zuul

---
+++ ZuulTest/src/main/java/com/zuul/boot/ZuulServer.java
@@ -0,0 +1,50 @@
+package com.zuul.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZuulServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServer.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
+
+class SimpleFilter extends ZuulFilter {
+
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		//HttpServletRequest request = ctx.getRequest();
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
